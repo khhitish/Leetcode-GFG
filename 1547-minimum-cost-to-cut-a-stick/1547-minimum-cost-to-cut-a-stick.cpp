9f45245834eadf3f011c6741579c0599d5bbad67
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-13 12:41:28 +0530

    Time: 59 ms (85.64%), Space: 10.3 MB (38.67%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -3,10 +3,10 @@ class Solution
     private:
         int helper(int i, int j, vector<int> &cuts, vector<vector< int>> &dp)
         {
-            if (i == j-1) return 0;
+            if (i == j - 1) return 0;
             if (dp[i][j] != -1) return dp[i][j];
             int ans = 1e9;
-            for (int k = i+1; k < j; k++)
+            for (int k = i + 1; k < j; k++)
             {
                 int currcost = cuts[j] - cuts[i] + helper(i, k, cuts, dp) + helper(k, j, cuts, dp);
                 ans = min(ans, currcost);
@@ -20,7 +20,27 @@ class Solution
             cuts.push_back(n);
             int m = cuts.size();
             sort(cuts.begin(), cuts.end());
-            vector<vector < int>> dp(m,vector<int> (m, -1));
-            return helper(0, m - 1, cuts, dp);
+            // vector<vector < int>> dp(m, vector<int> (m, -1));
+            // return helper(0, m - 1, cuts, dp);
+            
+            vector<vector<int>> dp(m, vector<int> (m,0));
+            for(int i=m-1;i>=0;i--)
+            {
+                for(int j=i+1; j<=m-1;j++)
+                {
+                    if(i==j-1) dp[i][j]=0; // base case
+                    else
+                    {
+                        int ans=1e9;
+                        for(int k=i+1;k<j;k++)
+                        {
+                            int currcost = cuts[j] - cuts[i] + dp[i][k] + dp[k][j];
+                            ans=min(ans,currcost);
+                        }
+                        dp[i][j]=ans;
+                    }
+                }
+            }
+            return dp[0][m-1];
         }
 };
