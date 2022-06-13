781ed6a9852936952808abab63438418f9b7402d
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-13 12:44:45 +0530

    Time: 76 ms (71.28%), Space: 10.3 MB (62.69%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,6 +1,7 @@
 class Solution
 {
     private:
+       	// memoization
         int helper(int i, int j, vector<int> &cuts, vector<vector< int>> &dp)
         {
             if (i == j - 1) return 0;
@@ -20,27 +21,30 @@ class Solution
             cuts.push_back(n);
             int m = cuts.size();
             sort(cuts.begin(), cuts.end());
-            // vector<vector < int>> dp(m, vector<int> (m, -1));
-            // return helper(0, m - 1, cuts, dp);
-            
-            vector<vector<int>> dp(m, vector<int> (m,0));
-            for(int i=m-1;i>=0;i--)
+
+           	// memoization
+           	// vector<vector < int>> dp(m, vector<int> (m, -1));
+           	// return helper(0, m - 1, cuts, dp);
+
+           	// tabulation
+            vector<vector < int>> dp(m, vector<int> (m, 0));
+            for (int i = m - 1; i >= 0; i--)
             {
-                for(int j=i+1; j<=m-1;j++)
+                for (int j = i + 1; j <= m - 1; j++)
                 {
-                    if(i==j-1) dp[i][j]=0; // base case
+                    if (i == j - 1) dp[i][j] = 0;	// base case
                     else
                     {
-                        int ans=1e9;
-                        for(int k=i+1;k<j;k++)
+                        int ans = 1e9;
+                        for (int k = i + 1; k < j; k++)
                         {
                             int currcost = cuts[j] - cuts[i] + dp[i][k] + dp[k][j];
-                            ans=min(ans,currcost);
+                            ans = min(ans, currcost);
                         }
-                        dp[i][j]=ans;
+                        dp[i][j] = ans;
                     }
                 }
             }
-            return dp[0][m-1];
+            return dp[0][m - 1];
         }
 };
