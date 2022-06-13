beb40b2f323f1323cca5d38e7f09925fa3ae6c3b
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-14 00:04:29 +0530

    Time: 74 ms (90.39%), Space: 23.7 MB (72.15%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -1,5 +1,6 @@
 class Solution
 {
+    // https://youtu.be/auS1fynpnjo
     public:
         int countSquares(vector<vector < int>> &matrix)
         {
@@ -25,45 +26,45 @@ class Solution
            	// return ans;
 
            	// space optimised tabulation
-           	vector<int> dp(n);
-           	int prev = 0;
-           	int ans = 0;
-           	for (int i = 0; i < m; i++)
-           	{
-           	    for (int j = 0; j < n; j++)
-           	    {
-           	        int temp = dp[j];
-           	        if (i == 0 || j == 0 || matrix[i][j] == 0)
-           	        {
-           	            dp[j] = matrix[i][j];
-           	        }
-           	        else
-           	        {
-           	            dp[j] = min(dp[j], min(prev, dp[j - 1])) + 1;
-           	        }
-           	        ans += dp[j];
-           	        prev = temp;
-           	    }
-           	}
-           	return ans;
+           	// vector<int> dp(n);
+           	// int prev = 0;
+           	// int ans = 0;
+           	// for (int i = 0; i < m; i++)
+           	// {
+           	//     for (int j = 0; j < n; j++)
+           	//     {
+           	//         int temp = dp[j];
+           	//         if (i == 0 || j == 0 || matrix[i][j] == 0)
+           	//         {
+           	//             dp[j] = matrix[i][j];
+           	//         }
+           	//         else
+           	//         {
+           	//             dp[j] = min(dp[j], min(prev, dp[j - 1])) + 1;
+           	//         }
+           	//         ans += dp[j];
+           	//         prev = temp;
+           	//     }
+           	// }
+           	// return ans;
 
            	// reusing the matrix constant auxiliary space
-            // int ans = 0;
-            // for (int i = 0; i < m; i++)
-            // {
-            //     for (int j = 0; j < n; j++)
-            //     {
-            //         if (i == 0 || j == 0 || matrix[i][j] == 0)
-            //         {
-            //             matrix[i][j] = matrix[i][j];
-            //         }
-            //         else
-            //         {
-            //             matrix[i][j] = min(matrix[i - 1][j], min(matrix[i - 1][j - 1], matrix[i][j - 1])) + 1;
-            //         }
-            //         ans += matrix[i][j];
-            //     }
-            // }
-            // return ans;
+            int ans = 0;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == 0 || j == 0 || matrix[i][j] == 0)
+                    {
+                        matrix[i][j] = matrix[i][j];
+                    }
+                    else
+                    {
+                        matrix[i][j] = min(matrix[i - 1][j], min(matrix[i - 1][j - 1], matrix[i][j - 1])) + 1;
+                    }
+                    ans += matrix[i][j];
+                }
+            }
+            return ans;
         }
 };
