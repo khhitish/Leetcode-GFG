2f6b47d696175323beb7beb53cf228a5391d78c0
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-16 18:16:44 +0530

    Time: 821 ms (14.38%), Space: 30.2 MB (32.33%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,63 +1,87 @@
 class Solution {
 private:
-    bool helper(int i, int j, string& s, vector<vector<int>>& dp, int& maxlen, int& start)
-    {
-        if(i>j) return true;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==s[j])
-        {
-            dp[i][j] = helper(i+1,j-1,s,dp,maxlen,start);
-        }
-        else
-        {
-            dp[i][j]=false;
-        }
-        if(dp[i][j]==1)
-        {
-            if(maxlen < j-i+1)
-            {
-                maxlen=j-i+1;
-                start=i;
-            }
-        }
-        return dp[i][j];
-    }
+    // // memoization
+    // bool helper(int i, int j, string& s, vector<vector<int>>& dp, int& maxlen, int& start)
+    // {
+    //     if(i>j) return true;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(s[i]==s[j])
+    //     {
+    //         dp[i][j] = helper(i+1,j-1,s,dp,maxlen,start);
+    //     }
+    //     else
+    //     {
+    //         dp[i][j]=false;
+    //     }
+    //     if(dp[i][j]==1)
+    //     {
+    //         if(maxlen < j-i+1)
+    //         {
+    //             maxlen=j-i+1;
+    //             start=i;
+    //         }
+    //     }
+    //     return dp[i][j];
+    // }
 public:
     string longestPalindrome(string s) {
         int n=s.size();
-        int start=0, maxlen=0;
+        int start=0, maxlen=1;
+        
+        // // memoization way of precomputing palindromes and simultaneously getting our ans
+        // // Time = O(n^2) Space = O(n^2)
+        // vector<vector<int>> dp(n,vector<int> (n,-1));
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=i;j<n;j++)
+        //     {
+        //         helper(i,j,s,dp,maxlen,start);
+        //     }
+        // }
+        // return s.substr(start,maxlen);
         
         
-        vector<vector<int>> dp(n,vector<int> (n,-1));
-        for(int i=0;i<n;i++)
+        // tabulation way of precomputing palindromes and simultaneously getting our ans
+        // Time = O(n^2) Space = O(n^2)
+        vector<vector<bool>> dp(n, vector<bool> (n,false));
+        for(int i=0;i<n;i++) // base cases (i,i) & (i,i+1)
         {
-            for(int j=i;j<n;j++)
+            dp[i][i]=true; // length 1 substrings
+            int j=i+1;
+            if(j<n) // length 2 substrings
             {
-                helper(i,j,s,dp,maxlen,start);
+                if(s[i]==s[j])
+                {
+                    dp[i][j]=true;
+                    if(maxlen<j-i+1)
+                    {
+                        maxlen=j-i+1;
+                        start=i;
+                    }
+                }
+            }
+        }
+        for(int len=3;len<=n;len++) // length 3 onwards, we have to do it in the increasing order of length so that we can use the previous results in calculating curr results
+        {
+            for(int i=0;i<n-len+1;i++)//so that j doesnt go out of bounds
+            {
+                int j=i+len-1;
+                if(s[i]==s[j] && dp[i+1][j-1]==true)
+                {
+                    dp[i][j]=true;
+                    if(maxlen<j-i+1)
+                    {
+                        maxlen=j-i+1;
+                        start=i;
+                    }
+                }
+                else
+                {
+                    dp[i][j]=false;
+                }
+                
             }
         }
         return s.substr(start,maxlen);
-        // pair<int,int> ans;
-        // for(int i=0;i<n;i++)
-        // {
-        //     string temp;
-        //     for(int j=i+1;j<n;j++)
-        //     {
-        //         if(dp[i][j]==true)
-        //         {
-        //             if(ans.second-ans.first + 1 <j-i+1)
-        //             {
-        //                 ans = {i,j};
-        //             }
-        //         }
-        //     }
-        // }
-        // string res;
-        // for(int i=ans.first;i<=ans.second;i++)
-        // {
-        //     res+=s[i];
-        // }
-        // return res;
-        
     }
 };
