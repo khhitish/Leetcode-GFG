43c5dc570dc52454e3dd9f7bb5f929b2003edf1b
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-16 17:32:11 +0530

    Time: 2811 ms (5.01%), Space: 387.2 MB (5.33%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,73 +1,63 @@
 class Solution {
 private:
-    bool helper(int i, int j, string& s, vector<vector<int>>& dp)
+    bool helper(int i, int j, string& s, vector<vector<int>>& dp, int& maxlen, int& start)
     {
         if(i>j) return true;
         if(dp[i][j]!=-1) return dp[i][j];
         if(s[i]==s[j])
         {
-            dp[i][j] = helper(i+1,j-1,s,dp);
+            dp[i][j] = helper(i+1,j-1,s,dp,maxlen,start);
         }
         else
         {
             dp[i][j]=false;
+        }
+        if(dp[i][j]==1)
+        {
+            if(maxlen < j-i+1)
+            {
+                maxlen=j-i+1;
+                start=i;
+            }
         }
         return dp[i][j];
     }
 public:
     string longestPalindrome(string s) {
         int n=s.size();
+        int start=0, maxlen=0;
+        
+        
         vector<vector<int>> dp(n,vector<int> (n,-1));
-        // for(int i=n-1;i>=0;i--)
-        // {
-        //     for(int j=i;j<n;j++)
-        //     {
-        //         if(s[i]==s[j])
-        //         {
-        //             int x=i+1, y=j-1;
-        //             if(x<y || x>=n || j<0)
-        //             {
-        //                 dp[i][j]=true;
-        //             }
-        //             else
-        //             {
-        //                 if(dp[x][y]==true) dp[i][j]=true;
-        //             }
-        //         }
-        //         else
-        //         {
-        //             dp[i][j]=false;
-        //         }
-        //     }
-        // }
         for(int i=0;i<n;i++)
         {
             for(int j=i;j<n;j++)
             {
-                helper(i,j,s,dp);
+                helper(i,j,s,dp,maxlen,start);
             }
         }
-        pair<int,int> ans;
-        for(int i=0;i<n;i++)
-        {
-            string temp;
-            for(int j=i+1;j<n;j++)
-            {
-                if(dp[i][j]==true)
-                {
-                    if(ans.second-ans.first + 1 <j-i+1)
-                    {
-                        ans = {i,j};
-                    }
-                }
-            }
-        }
-        string res;
-        for(int i=ans.first;i<=ans.second;i++)
-        {
-            res+=s[i];
-        }
-        return res;
+        return s.substr(start,maxlen);
+        // pair<int,int> ans;
+        // for(int i=0;i<n;i++)
+        // {
+        //     string temp;
+        //     for(int j=i+1;j<n;j++)
+        //     {
+        //         if(dp[i][j]==true)
+        //         {
+        //             if(ans.second-ans.first + 1 <j-i+1)
+        //             {
+        //                 ans = {i,j};
+        //             }
+        //         }
+        //     }
+        // }
+        // string res;
+        // for(int i=ans.first;i<=ans.second;i++)
+        // {
+        //     res+=s[i];
+        // }
+        // return res;
         
     }
 };
