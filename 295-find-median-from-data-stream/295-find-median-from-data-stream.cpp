6d53ad8b324453a308923dd8904657a090609ec8
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-18 22:33:26 +0530

    Time: 577 ms (24.58%), Space: 117.1 MB (20.16%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -1,15 +1,21 @@
-class MedianFinder {
-private:
-    double median;
+// https://youtu.be/RrxpTWqj97A
+class MedianFinder
+{
+    private:
+        double median;
     priority_queue<int> maxheap;
-    priority_queue<int, vector<int>, greater<int>> minheap;
-public:
-    MedianFinder() {
-        
-    }
-    
-    void addNum(int num) {
-        if(num>median)
+    priority_queue<int, vector < int>, greater < int>> minheap;
+    public:
+        MedianFinder()
+        {
+            median = 0;
+        }
+
+    void addNum(int num)
+    {
+
+       	// inserting new element
+        if (num > median)
         {
             minheap.push(num);
         }
@@ -17,41 +23,48 @@ public:
         {
             maxheap.push(num);
         }
-        int s1=maxheap.size(), s2=minheap.size();
-        if(s1-s2>1)
+
+       	// maintaining a size difference of 1 
+        int s1 = maxheap.size(), s2 = minheap.size();
+        if (s1 - s2 > 1)
         {
             minheap.push(maxheap.top());
             maxheap.pop();
-            s2++;s1--;
+            s2++;
+            s1--;
         }
-        else if(s2-s1>1)
+        else if (s2 - s1 > 1)
         {
             maxheap.push(minheap.top());
             minheap.pop();
-            s2--; s1++;
+            s2--;
+            s1++;
         }
-        if(s1==s2)
+
+       	// updating median
+        if (s1 == s2)
         {
-            median= (minheap.top() + maxheap.top())/(2.0);
+            median = (minheap.top() + maxheap.top()) / (2.0);
         }
-        else if(s1>s2)
+        else if (s1 > s2)
         {
-            median=maxheap.top();
+            median = maxheap.top();
         }
         else
         {
-            median=minheap.top();
+            median = minheap.top();
         }
     }
-    
-    double findMedian() {
+
+    double findMedian()
+    {
         return median;
     }
 };
 
 /**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
+ *Your MedianFinder object will be instantiated and called as such:
+ *MedianFinder* obj = new MedianFinder();
+ *obj->addNum(num);
+ *double param_2 = obj->findMedian();
  */
