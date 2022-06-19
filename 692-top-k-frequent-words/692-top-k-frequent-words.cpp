9d1f60880cd27ce221a64921d27f90df3fa55ab0
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-19 13:26:05 +0530

    Time: 25 ms (24.40%), Space: 12.6 MB (51.45%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,5 +1,6 @@
 struct compare 
 {
+    // implementing minheap only but when freq is same lexicographically larger element goes towards the top so after reversing ans array: highest freq elements in the beginning and if 2 have same frequency lexicographically smaller one comes first
     bool operator()(pair<int,string> &p1, pair<int,string> &p2)
     {
         if(p1.first>p2.first) return true;
