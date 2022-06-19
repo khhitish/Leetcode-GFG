d6b139359949bc990de28863b5f24a399bd1b10c
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-19 11:25:37 +0530

    Time: 283 ms (31.91%), Space: 77.2 MB (43.98%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -30,89 +30,72 @@ class Trie
     private:
         Node * root;
     public:
-    Trie()
-    {
-        root= new Node();
-    }
+        Trie()
+        {
+            root = new Node();
+        }
     void insert(string word)
     {
-        Node* node=root;
-        for(int i=0;i<word.size();i++)
+        Node *node = root;
+        for (int i = 0; i < word.size(); i++)
         {
-            if(node->containsKey(word[i])==false)
+            if (node->containsKey(word[i]) == false)
             {
                 node->put(word[i], new Node());
             }
-            node=node->get(word[i]);
+            node = node->get(word[i]);
         }
         node->setEnd();
     }
-    void helper(Node* node, string& sol, vector<string>& ans)
+    void helper(Node *node, string &sol, vector<string> &ans)
     {
-        if(node->isEnd())
+        if (node->isEnd())
         {
             ans.push_back(sol);
         }
-        for(int i=0;i<26;i++)
+        for (int i = 0; i < 26; i++)
         {
-            if(ans.size()==3) return;
-            char curr='a' + i;
-            if(node->containsKey(curr))
+            if (ans.size() == 3) return;
+            char curr = 'a' + i;
+            if (node->containsKey(curr))
             {
                 sol.push_back(curr);
-                helper(node->get(curr),sol,ans);
+                helper(node->get(curr), sol, ans);
                 sol.pop_back();
             }
         }
     }
-    void printall(string& prefix, vector<string>& ans)
+    void printall(string &prefix, vector<string> &ans)
     {
-        Node* node=root;
-        for(int i=0;i<prefix.size();i++)
+        Node *node = root;
+        for (int i = 0; i < prefix.size(); i++)
         {
-            if(!node->containsKey(prefix[i])) return;
-            node=node->get(prefix[i]);
+            if (!node->containsKey(prefix[i])) return;
+            node = node->get(prefix[i]);
         }
-        string sol=prefix;
-        helper(node,sol,ans);
+        string sol = prefix;
+        helper(node, sol, ans);
     }
 };
 class Solution
 {
     public:
-        vector<vector < string>> suggestedProducts(vector<string> &products, string searchWord) {
-            Trie* t = new Trie();
-            for(auto&x : products)
+        vector<vector < string>> suggestedProducts(vector<string> &products, string searchWord)
+        {
+            Trie *t = new Trie();
+            for (auto &x: products)
             {
                 t->insert(x);
             }
             string s;
-            vector<vector<string>> ans;
-            for(auto&x : searchWord)
+            vector<vector < string>> ans;
+            for (auto &x: searchWord)
             {
-                s+=x;
+                s += x;
                 vector<string> temp;
-                t->printall(s,temp);
+                t->printall(s, temp);
                 ans.push_back(temp);
             }
             return ans;
         }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
