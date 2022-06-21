ef68028494b28239b949638643458d878974b223
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-21 19:50:18 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (74.43%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int n5=0;
-        for(int i=1;i<=n;i++)
+        
+        // time: O(nlog5(n))
+        // int n5=0;
+        // for(int i=1;i<=n;i++)
+        // {
+        //     int t=i;
+        //     while(t%5==0)
+        //     {
+        //         n5++;
+        //         t/=5;
+        //     }
+        // }
+        // return n5;
+        
+        // time= O(log5(n))
+        
+        int ans=0;
+        int i=5;
+        while(i<=n)
         {
-            int t=i;
-            while(t%5==0)
-            {
-                n5++;
-                t/=5;
-            }
+            ans+=(n/i);
+            i*=5;
         }
-        return n5;
+        return ans;
     }
 };
