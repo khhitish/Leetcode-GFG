b1914f2a41b53f3a30f38c7fbbe5260ee64a172c
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-21 19:55:05 +0530

    Time: 2 ms (57.26%), Space: 5.8 MB (74.43%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
@@ -1,4 +1,6 @@
 class Solution {
+    // https://youtu.be/zUuLh6x1i6M
+    // https://leetcode.com/problems/factorial-trailing-zeroes/discuss/52373/Simple-CC++-Solution-(with-detailed-explaination)
 public:
     int trailingZeroes(int n) {
         
@@ -16,7 +18,8 @@ public:
         // return n5;
         
         // time= O(log5(n))
-        
+        // we need to check number of 5s in all numbers starting from 1 to n. prev approach did this in nlogn time, this one does the same in logn time
+        //we know that we encounter a number divisible by 5 every 5 steps, so if we divide n by 5 we will get number of numbers from 1 to n divisible by 5, but then 25 has 2 fives, wo we account 1 5 by taking n/5 and another 5 of 25 by taking n/25. 125 has 3 5s 2 are taken care of by 5 and 25 another one by 125, similarly we keep doing this as lo g as denominator is less than equal to n, i.e. we do this for log5(n) times
         int ans=0;
         int i=5;
         while(i<=n)
