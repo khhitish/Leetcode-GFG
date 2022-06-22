7f9f535682f00830a8bade0e190e96e3aaf292f9
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-22 21:16:40 +0530

    Time: 8 ms (16.93%), Space: 9.1 MB (29.10%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -13,10 +13,10 @@ public:
                 nums1[k--]=nums2[j--];
             }
         }
-        while(i>=0)
-        {
-            nums1[k--]=nums1[i--];
-        }
+        // while(i>=0)
+        // {
+        //     nums1[k--]=nums1[i--];
+        // }
         while(j>=0)
         {
             nums1[k--]=nums2[j--];
