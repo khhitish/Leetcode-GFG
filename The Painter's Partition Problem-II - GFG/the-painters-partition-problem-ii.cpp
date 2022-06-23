9d6626d843e5ccac4a2eabc44d9b42f8177f70f4
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-23 18:00:15 +0530

    Added solution - LeetHub

--- The Painter's Partition Problem-II - GFG/the-painters-partition-problem-ii.cpp
+++ The Painter's Partition Problem-II - GFG/the-painters-partition-problem-ii.cpp
@@ -11,84 +11,85 @@ using namespace std;
 class Solution
 {
   public:
-    long long minTime(int a[], int n, int k)
+    long long minTime(int arr[], int n, int k)
     {
-        // long long low = *max_element(arr,arr+n);
-        // long long high = accumulate(arr,arr+n,0);
-        // //long long ans;
-        // while(low<high)
-        // {
-        //     long long mid= low + (high-low)/2;
-        //     // checking if mid time is possible
-        //     long long partitions=1;
-        //     long long tempsum=0;
-        //     for(int i=0;i<n;i++)
-        //     {
-        //         if(tempsum+arr[i]>mid)
-        //         {
-        //             partitions++;
-        //             tempsum=0;
-        //         }
-        //         tempsum+=arr[i];
-        //     }
+        long long low = *max_element(arr,arr+n);
+        long long high = accumulate(arr,arr+n,0LL);
+        //long long ans;
+        while(low<high)
+        {
+            long long mid= low + (high-low)/2;
+            // checking if mid time is possible
+            long long partitions=1;
+            long long tempsum=0;
+            for(int i=0;i<n;i++)
+            {
+                if(tempsum+arr[i]>mid)
+                {
+                    partitions++;
+                    tempsum=0;
+                }
+                tempsum+=arr[i];
+            }
             
-        //     if(partitions>k)
-        //     {
-        //         low=mid+1;
-        //     }
-        //     else
-        //     {
-        //         high=mid;
-        //     }
-        // }
-        // return low;
-         long long low= *max_element(a, a+n);
+            if(partitions>k)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid;
+            }
+        }
+        return low;
+        
+        //  long long low= *max_element(a, a+n);
 
-        long long high=0;
+        // long long high=0;
 
-        for(int i=0; i<n; i++)
+        // for(int i=0; i<n; i++)
 
-        high+=a[i];
+        // high+=a[i];
 
         
 
-        while(low<high)
+        // while(low<high)
 
-        {
+        // {
 
-            long long mid= low+(high-low)/2;
+        //     long long mid= low+(high-low)/2;
 
-            long long painter=1, t=0;
+        //     long long painter=1, t=0;
 
-            for(int i=0; i<n; i++)
+        //     for(int i=0; i<n; i++)
 
-            {
+        //     {
 
-                t+=a[i];
+        //         t+=a[i];
 
-                if(t>mid)
+        //         if(t>mid)
 
-                {
+        //         {
 
-                    t=a[i];
+        //             t=a[i];
 
-                    painter++;
+        //             painter++;
 
-                }
+        //         }
 
-            }
+        //     }
 
-            if(painter>k)
+        //     if(painter>k)
 
-            low= mid+1;
+        //     low= mid+1;
 
-            else
+        //     else
 
-            high=mid;
+        //     high=mid;
 
-        }
+        // }
 
-        return low;
+        // return low;
     }
 };
 
