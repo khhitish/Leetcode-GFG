e077990b80b723620fedb92a1b954a8f8d0b191b
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-24 20:44:14 +0530

    Time: 118 ms (36.67%), Space: 48.4 MB (77.59%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reachable=0, n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(i>reachable) return false;
+            reachable=max(reachable, nums[i] + i);
+        }
+        return true;
+    }
+};
