40520e5abc6ae8fd4526cbd0d3a3ae6b92b0347e
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-25 13:34:08 +0530

    Time: 0 ms (100.00%), Space: 11.2 MB (28.89%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,16 +1,17 @@
 class Solution
 {
     private:
+       	// https://www.ideserve.co.in/learn/find-pivot-in-a-sorted-rotated-array
         int findpivot(vector<int> &nums, int low, int high)
         {
             if (nums[low] < nums[high]) return -1;
             while (low <= high)
             {
                 int mid = low + (high - low) / 2;
-                if (mid < high && nums[mid] > nums[mid + 1]) return mid;
-                else if (mid > low && nums[mid - 1] > nums[mid]) return mid - 1;
-                else if (nums[mid] >= nums[low]) low = mid + 1;
-                else high = mid - 1;
+                if (mid < high && nums[mid] > nums[mid + 1]) return mid;	// mid is the pivot
+                else if (mid > low && nums[mid - 1] > nums[mid]) return mid - 1;	// mid-1 is the pivot
+                else if (nums[mid] >= nums[low]) low = mid + 1;	//low to mid sorted means no pivot bw low and mid
+                else high = mid - 1;	//low to mid not sorted means pivot is there means ignore mid to high part
             }
             return -1;
         }
