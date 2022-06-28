e20d96b00c8d3748356c74ae2cf5e3ec0c3a5948
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-28 21:37:13 +0530

    Added solution - LeetHub

---
+++ Check if a string is Isogram or not - GFG/check-if-a-string-is-isogram-or-not.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to check if a string is Isogram or not.
+    bool isIsogram(string s)
+    {
+        vector<int> freq(26,0);
+        for(auto&x : s)
+        {
+            freq[x-'a']++;
+            if(freq[x-'a']>1) return false;
+        }
+        return true;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        string s;
+        cin>>s;
+        Solution obj;
+        cout<<obj.isIsogram(s)<<endl;
+        
+    }
+	return 0;
+}  // } Driver Code Ends
