13da73abe4746c1ba036a4d4ef57e6f4ca3ef193
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-28 09:57:48 +0530

    Time: 63 ms (79.53%), Space: 17.3 MB (58.78%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -6,21 +6,34 @@ public:
         {
             freq[x-'a']++;
         }
-        map<int,int> mpp;
-        for(auto&x : freq)
+        // map<int,int> mpp;
+        // for(auto&x : freq)
+        // {
+        //     if(x>0) mpp[x]++;
+        // }
+        // int ans = 0;
+        // for(auto it=mpp.rbegin(); it!=mpp.rend(); it++)
+        // {
+        //     if(it->first==0) break;
+        //     if(it->second>1)
+        //     {
+        //         ans+=it->second-1;
+        //         //cout<<ans<<" ";
+        //         mpp[it->first-1]+=it->second-1;
+        //     }
+        // }
+        // return ans;
+        int ans=0;
+        sort(freq.begin(), freq.end(), greater<int>());
+        unordered_set<int> st;
+        for(int i=0;i<26 && freq[i]>0; i++)
         {
-            if(x>0) mpp[x]++;
-        }
-        int ans = 0;
-        for(auto it=mpp.rbegin(); it!=mpp.rend(); it++)
-        {
-            if(it->first==0) break;
-            if(it->second>1)
+            while(st.count(freq[i])>0)
             {
-                ans+=it->second-1;
-                //cout<<ans<<" ";
-                mpp[it->first-1]+=it->second-1;
+                ans++;
+                freq[i]--;
             }
+            if(freq[i]!=0) st.insert(freq[i]);
         }
         return ans;
     }
