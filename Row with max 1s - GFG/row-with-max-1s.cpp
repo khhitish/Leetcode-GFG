9688f38db2e195e0e341e49047c18cd2f8161469
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-28 15:30:26 +0530

    Added solution - LeetHub

---
+++ Row with max 1s - GFG/row-with-max-1s.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+	    int maxi=0, row=-1;
+	    for(int i=0;i<n;i++)
+	    {
+	        int curr = m - (lower_bound(arr[i].begin(), arr[i].end(), 1) - arr[i].begin());
+	        if(curr>maxi)
+	        {
+	            row=i;
+	            maxi=curr;
+	        }
+	    }
+	    return row;
+	}
+
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector< vector<int> > arr(n,vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin>>arr[i][j];
+            }
+        }
+        Solution ob;
+        auto ans = ob.rowWithMax1s(arr, n, m);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
