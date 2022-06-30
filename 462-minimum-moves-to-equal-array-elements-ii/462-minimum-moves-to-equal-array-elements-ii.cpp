fddda34056bc5f43f1db824ab96fb003665ee17b
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-06-30 08:58:44 +0530

    Time: 7 ms (97.98%), Space: 10.8 MB (92.09%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
+    // https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/discuss/1217473/C++PythonJava-2-Solutions-(w-and-wo-Median)-Explained-with-Example-implementation
     int minMoves2(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int left=0, right=nums.size()-1;
         int ans=0;
-        int y = nums[nums.size()/2];
-        for(auto&x : nums)
+        sort(nums.begin(), nums.end());
+        while(left<right)
         {
-            ans+=abs(x-y);
+            ans+=(nums[right]-nums[left]);
+            left++; right--;
         }
         return ans;
     }
