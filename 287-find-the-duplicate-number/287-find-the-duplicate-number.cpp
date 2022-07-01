64e5e69ab77a86bbafa63b98ecff7ae6424e8d18
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-01 13:53:28 +0530

    Time: 128 ms (85.98%), Space: 61.2 MB (61.14%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,45 +1,45 @@
-class Solution {
-public:
-    int findDuplicate(vector<int>& nums) {
-        // without modifying array and using constant space: 2 approaches
-        
-        // // 1) binary search the answer time nlogn space 1
-        // int low=1, high = nums.size()-1, ans=-1;
-        // while(low<=high)
-        // {
-        //     int mid = low + (high-low)/2;
-        //     int countless=0;
-        //     for(auto&x : nums)
-        //     {
-        //         if(x<=mid) countless++;
-        //     }
-        //     if(countless > mid)
-        //     {
-        //         ans=mid;
-        //         high=mid-1;
-        //     }
-        //     else
-        //     {
-        //         low=mid+1;
-        //     }
-        // }
-        // return ans;
-        
-        // 2) Linked list cycle detection 2 //see notes or last approach in the following link
-        // https://leetcode.com/problems/find-the-duplicate-number/discuss/1892921/Java-9-Approaches-Count-+-Hash-+-Sort-+-Binary-Search-+-Bit-+-Fast-Slow-Pointers 
-        int slow=nums[0], fast=nums[slow];
-        while(slow!=fast)
+class Solution
+{
+    public:
+        int findDuplicate(vector<int> &nums)
         {
-            slow=nums[slow];
-            fast=nums[nums[fast]];
+           	// without modifying array and using constant space: 2 approaches
+
+           	//	// 1) binary search the answer time nlogn space 1
+           	// int low=1, high = nums.size()-1, ans=-1;
+           	// while(low<=high)
+           	// {
+           	//     int mid = low + (high-low)/2;
+           	//     int countless=0;
+           	//     for(auto&x : nums)
+           	//     {
+           	//         if(x<=mid) countless++;
+           	//     }
+           	//     if(countless > mid)
+           	//     {
+           	//         ans=mid;
+           	//         high=mid-1;
+           	//     }
+           	//     else
+           	//     {
+           	//         low=mid+1;
+           	//     }
+           	// }
+           	// return ans;
+
+           	// 2) Linked list cycle detection 2	//see notes or last approach in the following link
+           	// https://leetcode.com/problems/find-the-duplicate-number/discuss/1892921/Java-9-Approaches-Count-+-Hash-+-Sort-+-Binary-Search-+-Bit-+-Fast-Slow-Pointers 
+            int slow = nums[0], fast = nums[0];
+            do {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            } while (slow != fast);
+            slow = nums[0];
+            while (slow != fast)
+            {
+                slow = nums[slow];
+                fast = nums[fast];
+            }
+            return slow;
         }
-        slow=0;
-        while(slow!=fast)
-        {
-            slow=nums[slow];
-            fast=nums[fast];
-        }
-        return slow;
-        
-    }
 };
