11fd51c0cdeb94075aefd189722504e16efa14cb
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-01 13:06:09 +0530

    Time: 200 ms (40.27%), Space: 61.2 MB (61.14%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        // without modifying arry and using constant space 2 approaches
-        // binary search the answer time nlogn space 1
-        int low=1, high = nums.size()-1, ans=-1;
-        while(low<=high)
+        // without modifying array and using constant space: 2 approaches
+        
+        // // 1) binary search the answer time nlogn space 1
+        // int low=1, high = nums.size()-1, ans=-1;
+        // while(low<=high)
+        // {
+        //     int mid = low + (high-low)/2;
+        //     int countless=0;
+        //     for(auto&x : nums)
+        //     {
+        //         if(x<=mid) countless++;
+        //     }
+        //     if(countless > mid)
+        //     {
+        //         ans=mid;
+        //         high=mid-1;
+        //     }
+        //     else
+        //     {
+        //         low=mid+1;
+        //     }
+        // }
+        // return ans;
+        
+        // 2) Linked list cycle detection 2 //see notes or last approach in the following link
+        // https://leetcode.com/problems/find-the-duplicate-number/discuss/1892921/Java-9-Approaches-Count-+-Hash-+-Sort-+-Binary-Search-+-Bit-+-Fast-Slow-Pointers 
+        int slow=nums[0], fast=nums[slow];
+        while(slow!=fast)
         {
-            int mid = low + (high-low)/2;
-            int countless=0;
-            for(auto&x : nums)
-            {
-                if(x<=mid) countless++;
-            }
-            if(countless > mid)
-            {
-                ans=mid;
-                high=mid-1;
-            }
-            else
-            {
-                low=mid+1;
-            }
-            //cout<<ans<<" ";
+            slow=nums[slow];
+            fast=nums[nums[fast]];
         }
-        return ans;
+        slow=0;
+        while(slow!=fast)
+        {
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+        
     }
 };
