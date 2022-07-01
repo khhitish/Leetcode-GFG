3fac3574e631e6ccdd3e660c79fe11130a9b01ea
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-01 19:50:42 +0530

    Time: 4 ms (58.98%), Space: 8.8 MB (11.97%) - LeetHub

--- 274-h-index/274-h-index.cpp
+++ 274-h-index/274-h-index.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations.begin(), citations.end());
+        
+        // sort(citations.begin(), citations.end());
+        // int n=citations.size();
+        // int low=1, high=n;
+        // int ans=0;
+        // while(low<=high)
+        // {
+        //     int mid = low+(high-low)/2;
+        //     if(citations[n-mid]>=mid)
+        //     {
+        //         ans=mid;
+        //         low=mid+1;
+        //     }
+        //     else
+        //     {
+        //         high=mid-1;
+        //     }
+        // }
+        // return ans;
+        
         int n=citations.size();
-        int low=1, high=n;
-        int ans=0;
-        while(low<=high)
+        vector<int> count(n+1);
+        int sum=0;
+        for(int i=0;i<n;i++)
         {
-            int mid = low+(high-low)/2;
-            if(citations[n-mid]>=mid)
+            if(citations[i]>n) sum++;
+            else 
             {
-                ans=mid;
-                low=mid+1;
-            }
-            else
-            {
-                high=mid-1;
+                count[citations[i]]++;
             }
         }
-        return ans;
+        for(int i=n;i>=0;i--)
+        {
+            sum+=count[i];
+            if(sum>=i) return i;
+        }
+        return 0;
     }
 };
