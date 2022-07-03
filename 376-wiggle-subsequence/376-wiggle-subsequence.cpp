1f0f1217f0b4850c5878ec09302995e736715121
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-03 07:59:42 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (19.43%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -4,24 +4,32 @@ public:
         int n=nums.size();
         vector<int> up(n), down(n);
         up[0]=down[0]=1;
+        int uplast=1,downlast=1;
         for(int i=1;i<n;i++)
         {
             if(nums[i]>nums[i-1])
             {
-                up[i]=down[i-1]+1;
-                down[i]=down[i-1];
+                // up[i]=down[i-1]+1;
+                // down[i]=down[i-1];
+                uplast=downlast+1;
+                downlast=downlast;
             }
             else if(nums[i]<nums[i-1])
             {
-                up[i]=up[i-1];
-                down[i] = up[i-1]+1;
+                // up[i]=up[i-1];
+                // down[i] = up[i-1]+1;
+                uplast=uplast;
+                downlast=uplast+1;
             }
             else
             {
-                down[i]=down[i-1];
-                up[i]=up[i-1];
+                // down[i]=down[i-1];
+                // up[i]=up[i-1];
+                uplast=uplast;
+                downlast=downlast;
             }
         }
-        return max(up[n-1],down[n-1]);
+        // return max(up[n-1],down[n-1]);
+        return max(uplast,downlast);
     }
 };
