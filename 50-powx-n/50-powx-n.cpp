74afe841b8520ac67f91f435e45d406c1e520a47
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-03 18:32:24 +0530

    Time: 5 ms (19.55%), Space: 6 MB (43.11%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0) return 1;
+        if(n<0)
+        {
+            x=(1/x);
+            n=abs(n);
+        }
+        double res=1;
+        if(n%2==1)
+        {
+            res*=x;
+        }
+        double temp=myPow(x,n/2);
+        return res*temp*temp;
+    }
+};
