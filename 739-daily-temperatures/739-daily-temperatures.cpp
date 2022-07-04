c874acdf31d29c2a38c9cc03362025eac68eb4db
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-04 15:38:50 +0530

    Time: 203 ms (77.85%), Space: 85.1 MB (86.23%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -3,13 +3,33 @@ public:
     vector<int> dailyTemperatures(vector<int>& t) {
         int n=t.size();
         vector<int> ans(n);
-        stack<int> s;
+        
+        // stack based standard approach
+        // stack<int> s;
+        // for(int i=n-1;i>=0;i--)
+        // {
+        //     while(!s.empty() && t[s.top()]<=t[i])s.pop();
+        //     if(s.empty()) ans[i]=0;
+        //     else ans[i]=s.top()-i;
+        //     s.push(i);
+        // }
+        // return ans;
+        
+        //using ans array for information O(1) space, O(n) time see solution
+        int hottest=INT_MIN;
         for(int i=n-1;i>=0;i--)
         {
-            while(!s.empty() && t[s.top()]<=t[i])s.pop();
-            if(s.empty()) ans[i]=0;
-            else ans[i]=s.top()-i;
-            s.push(i);
+            if(t[i]>=hottest) ans[i]=0;
+            else
+            {
+                int check = i+1;
+                while(t[check]<=t[i])
+                {
+                    check+=ans[check];
+                }
+                ans[i] = (check-i);
+            }
+            hottest=max(hottest,t[i]);
         }
         return ans;
     }
