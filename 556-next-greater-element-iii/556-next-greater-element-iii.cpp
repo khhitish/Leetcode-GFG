3a7b3785cba9a255e3a1ed69fec60ec96fed71c4
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-04 14:34:38 +0530

    Time: 0 ms (100.00%), Space: 6 MB (67.46%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/556-next-greater-element-iii.cpp
@@ -1,10 +1,20 @@
-class Solution {
-public:
-    int nextGreaterElement(int n) {
-        string res=to_string(n);
-        next_permutation(res.begin(), res.end());
-        long long ans = stoll(res);
-        if(ans > INT_MAX || ans <= n) return -1;
-        return ans;
-    }
+class Solution
+{
+    public:
+        int nextGreaterElement(int n)
+        {
+            string res = to_string(n);
+            //next_permutation(res.begin(), res.end());
+            int m = res.size();
+            int i = m - 2;
+            while (i >= 0 && res[i] >= res[i + 1]) i--;
+            if (i < 0) return -1;
+            int j = m - 1;
+            while (j >= 0 && res[j] <= res[i]) j--;
+            swap(res[i], res[j]);
+            reverse(res.begin() + i + 1, res.end());
+            long long ans = stoll(res);
+            if (ans > INT_MAX) return -1;
+            return ans;
+        }
 };
