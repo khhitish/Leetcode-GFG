0c24d257ea75b633eed6e3f13e57fb46082b9715
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-04 22:52:48 +0530

    Time: 17 ms (92.67%), Space: 10.6 MB (44.70%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,17 +1,44 @@
 class Solution {
+    // https://leetcode.com/problems/sum-of-subarray-minimums/
+    
+    // https://leetcode.com/problems/sum-of-subarray-minimums/discuss/178876/stack-solution-with-very-detailed-explanation-step-by-step //see the end and comments or replies
+    
+    //https://leetcode.com/problems/sum-of-subarray-ranges/discuss/1626628/O(n)-solution-with-monotonous-stack-oror-Full-explaination
 public:
-    long long subArrayRanges(vector<int>& nums) {
+    long long subArrayRanges(vector<int>& arr) {
+        stack<long long> s;
         long long ans=0;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
+        int n=arr.size();
+        for(int i=0;i<=n;i++)
         {
-            int maxi=nums[i], mini=nums[i];
-            for(int j=i+1;j<n;j++)
+            while(!s.empty() && (i==n || arr[s.top()]<arr[i]))//i==n added to avoid writing another while loop
             {
-                maxi=max(maxi,nums[j]);
-                mini=min(mini,nums[j]);
-                ans+=(maxi-mini);
+                long long temp=s.top();
+                s.pop();
+                if(s.empty())
+                {
+                    ans+=arr[temp]*(temp-(-1))*(i-temp);
+                    continue;
+                }
+                ans+=arr[temp] * (temp-s.top()) *(i-temp);
             }
+            s.push(i);
+        }
+        while(!s.empty())s.pop();
+        for(int i=0;i<=n;i++)
+        {
+            while(!s.empty() && (i==n || arr[s.top()]>arr[i]))//i==n added to avoid writing another while loop
+            {
+                long long temp=s.top();
+                s.pop();
+                if(s.empty())
+                {
+                    ans-=arr[temp]*(temp-(-1))*(i-temp);
+                    continue;
+                }
+                ans-=arr[temp] * (temp-s.top()) *(i-temp);
+            }
+            s.push(i);
         }
         return ans;
     }
