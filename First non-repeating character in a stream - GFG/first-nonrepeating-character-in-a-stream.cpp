3739cbe9f7eabf7e6ac4ed4bfd78c278521fc3d8
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-06 13:47:43 +0530

    Added solution - LeetHub

--- First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
+++ First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
@@ -6,18 +6,54 @@ using namespace std;
 class Solution {
 	public:
 		string FirstNonRepeating(string s){
-		    queue<char> q;
-		    vector<int> v(26,0);
-		    string ans;
-		    for(int i=0;i<s.size();i++)
-		    {
-		        v[s[i]-'a']++;
-		        q.push(s[i]);
-		        while(!q.empty() && v[q.front()-'a']>1)q.pop();
-		        if(q.empty()) ans+='#';
-		        else ans+=q.front();
-		    }
-		    return ans;
+		    
+		    
+		  //  queue<char> q;
+		  //  vector<int> v(26,0);
+		  //  string ans;
+		  //  for(int i=0;i<s.size();i++)
+		  //  {
+		  //      v[s[i]-'a']++;
+		  //      q.push(s[i]);
+		  //      while(!q.empty() && v[q.front()-'a']>1)q.pop();
+		  //      if(q.empty()) ans+='#';
+		  //      else ans+=q.front();
+		  //  }
+		  //  return ans;
+		  
+		  vector<int> v(26,0);
+		  string ans;
+		  int track=0;
+		  for(int i=0;i<s.size();i++)
+		  {
+		      v[s[i]-'a']++;
+		      if(v[s[track]-'a']>1)
+		      {
+		          int j;
+		          for(j=track;j<=i;j++)
+		          {
+		              if(v[s[j]-'a']==1)
+		              {
+		                  track=j;
+		                  break;
+		              }
+		          }
+		          if(j==i+1)
+		          {
+		              ans+='#';
+		              track=j;
+		          }
+		          else
+		          {
+		              ans+=s[track];
+		          }
+		      }
+		      else
+		      {
+		          ans+=s[track];
+		      }
+		  }
+		  return ans;
 		}
 
 };
