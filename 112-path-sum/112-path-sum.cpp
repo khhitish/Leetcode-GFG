5b0793386b2b4398067c428f5281aaecafe01377
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-08 00:05:40 +0530

    Time: 31 ms (5.77%), Space: 21.4 MB (39.88%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -11,24 +11,29 @@
  */
 class Solution {
 private:
-    bool dfs(TreeNode* root, int& target, int& sum)
-    {
-        if(root==nullptr) return false;
-        if(root->left==nullptr && root->right==nullptr)
-        {
-            if(sum + root->val==target) return true;
-        }
-        sum+=root->val;
-        bool l = dfs(root->left,target,sum);
-        if(l==true) return true;
-        bool r = dfs(root->right,target,sum);
-        if(r==true) return true;
-        sum-=root->val;
-        return false;
-    }
+    // bool dfs(TreeNode* root, int& target, int& sum)
+    // {
+    //     if(root==nullptr) return false;
+    //     if(root->left==nullptr && root->right==nullptr)
+    //     {
+    //         if(sum + root->val==target) return true;
+    //     }
+    //     sum+=root->val;
+    //     bool l = dfs(root->left,target,sum);
+    //     if(l==true) return true;
+    //     bool r = dfs(root->right,target,sum);
+    //     if(r==true) return true;
+    //     sum-=root->val;
+    //     return false;
+    // }
 public:
     bool hasPathSum(TreeNode* root, int target) {
-        int sum=0;
-        return dfs(root,target,sum);
+        // int sum=0;
+        // return dfs(root,target,sum);
+        if(root==nullptr) return false;
+        if(root->left==nullptr && root->right==nullptr && target-root->val==0) return true;
+        bool checkleft = hasPathSum(root->left,target-root->val);
+        bool checkright = hasPathSum(root->right,target-root->val);
+        return checkleft || checkright;
     }
 };
