6cd76a763477244424e179b77b7736432eadc128
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-07 10:50:18 +0530

    Time: 4 ms (73.94%), Space: 6.3 MB (89.89%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -29,30 +29,58 @@ public:
         // return helper(m-1,s1,n-1,s2,o-1,s3,dp);
         
         // tabulation
-        vector<vector<bool>> dp(m+1, vector<bool> (n+1,false));
-        // if(dp[i-1][j] is true that means we have matched upto i-2th index of s1 and j-1th index of s2 because of index shifting) which means the index of s3 upto which we have matched will be: i-1+j letters means i+j-2th index, so now we need to match i+j-1th index
-        //if i=0,j=0 then we have reached this point after matching everything hence true
+        // vector<vector<bool>> dp(m+1, vector<bool> (n+1,false));
+        // // if(dp[i-1][j] is true that means we have matched upto i-2th index of s1 and j-1th index of s2 because of index shifting) which means the index of s3 upto which we have matched will be: i-1+j letters means i+j-2th index, so now we need to match i+j-1th index
+        // //if i=0,j=0 then we have reached this point after matching everything hence true
+        // for(int i=0;i<=m;i++)
+        // {
+        //     for(int j=0;j<=n;j++)
+        //     {
+        //         if(i==0 && j==0) dp[i][j]=true;
+        //         else if(i==0)
+        //         {
+        //             if(dp[i][j-1]==true && s2[j-1]==s3[i+j-1]) dp[i][j]=true;
+        //         }
+        //         else if(j==0)
+        //         {
+        //             if(dp[i-1][j]==true && s1[i-1]==s3[i+j-1]) dp[i][j]=true;
+        //         }
+        //         else
+        //         {
+        //             if((dp[i-1][j]==true && s1[i-1]==s3[i+j-1]) || 
+        //                (dp[i][j-1]==true && s2[j-1]==s3[i+j-1]))
+        //                 dp[i][j]=true;
+        //         }
+        //     }
+        // }
+        // return dp[m][n];
+        
+        // space optimised tabulation
+        vector<bool> dp(n+1,false);
         for(int i=0;i<=m;i++)
         {
             for(int j=0;j<=n;j++)
             {
-                if(i==0 && j==0) dp[i][j]=true;
+                if(i==0 && j==0) dp[j]=true;
                 else if(i==0)
                 {
-                    if(dp[i][j-1]==true && s2[j-1]==s3[i+j-1]) dp[i][j]=true;
+                    if(dp[j-1]==true && s2[j-1]==s3[i+j-1]) dp[j]=true;
+                    else dp[j]=false;
                 }
                 else if(j==0)
                 {
-                    if(dp[i-1][j]==true && s1[i-1]==s3[i+j-1]) dp[i][j]=true;
+                    if(dp[j]==true && s1[i-1]==s3[i+j-1]) dp[j]=true;
+                    else dp[j]=false;
                 }
                 else
                 {
-                    if((dp[i-1][j]==true && s1[i-1]==s3[i+j-1]) || 
-                       (dp[i][j-1]==true && s2[j-1]==s3[i+j-1]))
-                        dp[i][j]=true;
+                    if((dp[j]==true && s1[i-1]==s3[i+j-1]) || 
+                       (dp[j-1]==true && s2[j-1]==s3[i+j-1]))
+                        dp[j]=true;
+                    else dp[j]=false;
                 }
             }
         }
-        return dp[m][n];
+        return dp[n];
     }
 };
