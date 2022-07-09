a731134d5e11f592c75f282d1a06a333a67a90c9
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-09 12:16:47 +0530

    Time: 157 ms (31.65%), Space: 57 MB (49.08%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -12,29 +12,45 @@
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int target) {
-        TreeNode* curr=root;
-        TreeNode *node = new TreeNode(target);
-        if(root==nullptr) return node;
-        while(true)
+        // TreeNode* curr=root;
+        // TreeNode *node = new TreeNode(target);
+        // if(root==nullptr) return node;
+        // while(true)
+        // {
+        //     if(curr->val < target)
+        //     {
+        //         if(curr->right!=nullptr) curr=curr->right;
+        //         else
+        //         {
+        //             curr->right = node;
+        //             break;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         if(curr->left!=nullptr) curr=curr->left;
+        //         else
+        //         {
+        //             curr->left=node;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return root;
+        
+        //recursive
+        if(root==nullptr)
         {
-            if(curr->val < target)
-            {
-                if(curr->right!=nullptr) curr=curr->right;
-                else
-                {
-                    curr->right = node;
-                    break;
-                }
-            }
-            else
-            {
-                if(curr->left!=nullptr) curr=curr->left;
-                else
-                {
-                    curr->left=node;
-                    break;
-                }
-            }
+            TreeNode* p=new TreeNode(target);
+            return p;
+        }
+        if(root->val < target)
+        {
+            root->right = insertIntoBST(root->right,target);
+        }
+        else
+        {
+            root->left=insertIntoBST(root->left,target);
         }
         return root;
     }
