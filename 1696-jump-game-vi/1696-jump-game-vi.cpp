2363f0bb479d1edc911b5317512e8cc19371f1a2
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-09 10:44:47 +0530

    Time: 303 ms (52.62%), Space: 80.6 MB (76.55%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,36 +1,37 @@
-class Solution {
-private:
-    int helper(int i,int k, vector<int>& nums, vector<int>& dp)
-    {
-        if(i==0)
+class Solution
+{
+   	// https://leetcode.com/problems/sliding-window-maximum/
+    public:
+        int maxResult(vector<int> &nums, int k)
         {
-            return nums[0];
+            int n = nums.size();
+
+           	// priority queue to maintain a sliding window maximum of size k
+           	// n + nlogn time n space
+           	// vector<int> dp(n);
+           	// priority_queue<pair<int,int>> pq;
+           	// dp[0]=nums[0];
+           	// pq.push({dp[0],0});
+           	// for(int i=1;i < n;i++)
+           	// {
+           	//     while(!pq.empty() && pq.top().second < i-k)pq.pop();
+           	//     dp[i] = nums[i] + pq.top().first;
+           	//     pq.push({dp[i],i});
+           	// }
+           	// return dp[n-1];
+
+           	// using monotonic deque, decreasing from front to back
+            vector<int> dp(n);
+            dp[0] = nums[0];
+            deque<int> dq;
+            dq.push_back(0);
+            for (int i = 1; i < n; i++)
+            {
+                while (!dq.empty() && dq.front() < i - k) dq.pop_front();
+                dp[i] = nums[i] + dp[dq.front()];
+                while (!dq.empty() && dp[dq.back()] <= dp[i]) dq.pop_back();
+                dq.push_back(i);
+            }
+            return dp[n - 1];
         }
-        if(dp[i]!=-1) return dp[i];
-        int ans=INT_MIN;
-        for(int j=i-1;j>=max(0,i-k);j--)
-        {
-            ans=max(ans,nums[i] + helper(j,k,nums,dp));
-        }
-        return dp[i]=ans;
-    }
-        
-public:
-    int maxResult(vector<int>& nums, int k) {
-        int n=nums.size();
-        // vector<int> dp(n,-1);
-        // return helper(n-1,k,nums,dp);
-        
-        vector<int> dp(n);
-        priority_queue<pair<int,int>> pq;
-        dp[0]=nums[0];
-        pq.push({dp[0],0});
-        for(int i=1;i<n;i++)
-        {
-            while(!pq.empty() && pq.top().second<i-k)pq.pop();
-            dp[i] = nums[i] + pq.top().first;
-            pq.push({dp[i],i});
-        }
-        return dp[n-1];
-    }
 };
