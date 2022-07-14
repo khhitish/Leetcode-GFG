4411ae053517449dfa5dbec541e9df44594d2e90
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-14 09:07:05 +0530

    Time: 14 ms (96.05%), Space: 26.4 MB (28.28%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,33 +1,35 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-private:
-    TreeNode* helper(int prestart, int preend, int instart, int inend, vector<int>& preorder, vector<int>& inorder, unordered_map<int,int>& inmap)
-    {
-        if(prestart>preend) return nullptr;
-        int inroot = inmap[preorder[prestart]];
-        TreeNode* curr = new TreeNode(preorder[prestart]);
-        curr->left = helper(prestart+1, prestart + inroot - instart, instart, inroot-1, preorder,inorder, inmap);
-        curr->right = helper(prestart + inroot - instart + 1, preend, inroot+1,inend,preorder,inorder,inmap);
-        return curr;
-    }
-public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        unordered_map<int,int> inmap;
-        int n=inorder.size();
-        for(int i=0;i<n;i++)
+class Solution
+{
+    private:
+        TreeNode* helper(int prestart, int preend, int instart, int inend, vector<int> &preorder, vector<int> &inorder, unordered_map< int, int> &inmap)
         {
-            inmap[inorder[i]]=i;
+            if (prestart > preend) return nullptr;
+            int inroot = inmap[preorder[prestart]];
+            TreeNode *curr = new TreeNode(preorder[prestart]);
+            curr->left = helper(prestart + 1, prestart + inroot - instart, instart, inroot - 1, preorder, inorder, inmap);
+            curr->right = helper(prestart + inroot - instart + 1, preend, inroot + 1, inend, preorder, inorder, inmap);
+            return curr;
         }
-        return helper(0,n-1,0,n-1,preorder,inorder,inmap);
-    }
+    public:
+        TreeNode* buildTree(vector<int> &preorder, vector<int> &inorder)
+        {
+            unordered_map<int, int> inmap;
+            int n = inorder.size();
+            for (int i = 0; i < n; i++)
+            {
+                inmap[inorder[i]] = i;
+            }
+            return helper(0, n - 1, 0, n - 1, preorder, inorder, inmap);
+        }
 };
