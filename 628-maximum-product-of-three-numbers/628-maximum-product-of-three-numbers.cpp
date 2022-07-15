bad3e5076c8a03f5dcf824763ae89e76dc1e179a
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-15 20:02:21 +0530

    Time: 52 ms (85.94%), Space: 27.6 MB (79.42%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
@@ -1,15 +1,48 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n=nums.size();
-        if(nums[0]>=0 || nums[n-1]<0)
+        // sort(nums.begin(), nums.end());
+        // int n=nums.size();
+        // if(nums[0]>=0 || nums[n-1]<0)
+        // {
+        //     return nums[n-1] * nums[n-2] * nums[n-3];
+        // }
+        // else
+        // {
+        //     return max(nums[n-1]*nums[n-2]*nums[n-3], nums[0]*nums[1]*nums[n-1]);
+        // }
+        int max1,max2,max3;
+        max1=max2=max3=INT_MIN;
+        int min1,min2;
+        min1=min2=INT_MAX;
+        for(int i=0;i<nums.size();i++)
         {
-            return nums[n-1] * nums[n-2] * nums[n-3];
+            if(nums[i]>max1)
+            {
+                max3=max2;
+                max2=max1;
+                max1=nums[i];
+            }
+            else if(nums[i]>max2)
+            {
+                max3=max2;
+                max2=nums[i];
+            }
+            else if(nums[i]>max3)
+            {
+                max3=nums[i];
+            }
+            
+            if(nums[i]<min1)
+            {
+                min2=min1;
+                min1=nums[i];
+            }
+            else if(nums[i]<min2)
+            {
+                min2=nums[i];
+            }
         }
-        else
-        {
-            return max(nums[n-1]*nums[n-2]*nums[n-3], nums[0]*nums[1]*nums[n-1]);
-        }
+        return max(max1*max2*max3, min1*min2*max1);
     }
 };
