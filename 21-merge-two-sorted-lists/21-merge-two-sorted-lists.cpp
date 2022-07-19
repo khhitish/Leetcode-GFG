01f70179335eb423315a078247874330af759bf1
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-19 18:48:59 +0530

    Time: 12 ms (55.00%), Space: 14.9 MB (13.96%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -26,6 +26,7 @@ public:
                 l2=l2->next;
             }
             curr=curr->next;
+            curr->next=nullptr;
         }
         if(l1!=nullptr)
         {
@@ -36,5 +37,22 @@ public:
             curr->next=l2;
         }
         return dummy->next;
+        
+        // if(l1==nullptr && l2==nullptr) return nullptr;
+        // else if(l1==nullptr) return l2;
+        // else if(l2==nullptr) return l1;
+        // else
+        // {
+        //     if(l1->val<l2->val)
+        //     {
+        //         l1->next = mergeTwoLists(l1->next,l2);
+        //         return l1;
+        //     }
+        //     else
+        //     {
+        //         l2->next = mergeTwoLists(l1,l2->next);
+        //         return l2;
+        //     }
+        // }
     }
 };
