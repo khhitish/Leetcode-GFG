1880e433920af81a851e0010317ed3f58ab06729
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-21 16:54:30 +0530

    Time: 63 ms (40.63%), Space: 71.3 MB (69.50%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
@@ -49,11 +49,74 @@ private:
         }
         return curr;
     }
+    
+    int sizeofll(ListNode* head)
+    {
+        int size=0;
+        while(head!=nullptr)
+        {
+            head=head->next;
+            size++;
+        }
+        return size;
+    }
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* f=reverse(l1);
-        ListNode* s=reverse(l2);
-        ListNode* res = addTwoNos1(f,s);
-        return reverse(res);
+        
+        // // reversing input lists and adding like addtwonumbers1 question
+        // ListNode* f=reverse(l1);
+        // ListNode* s=reverse(l2);
+        // ListNode* res = addTwoNos1(f,s);
+        // return reverse(res);
+        
+        int s1=sizeofll(l1), s2=sizeofll(l2);
+        ListNode* prev=nullptr, *curr=nullptr;
+        while(l1!=nullptr && l2!=nullptr)
+        {
+            int sum=0;
+            if(s1==s2)
+            {
+                sum+=l1->val + l2->val;
+                l1=l1->next;
+                l2=l2->next;
+                s1--;
+                s2--;
+            }
+            else if(s1>s2)
+            {
+                sum+=l1->val;
+                l1=l1->next;
+                s1--;
+            }
+            else
+            {
+                sum+=l2->val;
+                l2=l2->next;
+                s2--;
+            }
+            curr=new ListNode(sum);
+            curr->next=prev;
+            prev=curr;
+        }
+        int carry=0;
+        ListNode* ahead=curr;
+        curr=nullptr; prev=nullptr;
+        while(ahead!=nullptr)
+        {
+            prev=curr;
+            curr=ahead;
+            ahead=ahead->next;
+            int sum = curr->val + carry;
+            curr->val = sum%10;
+            carry=sum/10;
+            curr->next=prev;
+        }
+        if(carry>0)
+        {
+            ListNode* res = new ListNode(carry);
+            res->next=curr;
+            return res;
+        }
+        return curr;
     }
 };
