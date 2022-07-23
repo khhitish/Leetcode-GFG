3832cc04ca777a9a5f6a7e701212190b39c16473
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-24 01:20:04 +0530

    Time: 242 ms (14.27%), Space: 21.8 MB (14.13%) - LeetHub

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,42 +1,35 @@
-// https://youtu.be/RrxpTWqj97A
-// Time: O(n*k*logk) worst case, space: O(k)
-class Solution
-{
-    public:
-        vector<int> smallestRange(vector<vector < int>> &nums)
+// https://youtu.be/RrxpTWqj97A?t=427
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        int mini=INT_MAX, maxi = INT_MIN;
+        vector<int> ans(2);
+        ans[0]=-1e9; ans[1]=1e9;
+        for(int i=0;i<nums.size();i++)
         {
-            priority_queue<vector < int>, vector< vector< int >>, greater<vector< int>>> pq;
-            int mini = INT_MAX, maxi = INT_MIN;
-            for (int i = 0; i < nums.size(); i++)
-            {
-                pq.push({ nums[i][0],
-                    i,
-                    0 });
-                maxi = max(maxi, nums[i][0]);
-            }
-            vector<int> ans(2);
-            ans[0] = -1e9;
-            ans[1] = 1e9;
-            while (true)
-            {
-                auto v = pq.top();
-                pq.pop();
-                mini = v[0];
-                int i = v[1], j = v[2];
-                if ((maxi - mini < ans[1] - ans[0]) || (maxi - mini == ans[1] - ans[0] && mini < ans[0]))
-                {
-                    ans[0] = mini;
-                    ans[1] = maxi;
-                }
-                if (j + 1 < nums[i].size())
-                {
-                    pq.push({ nums[i][j + 1],
-                        i,
-                        j + 1 });
-                    maxi = max(maxi, nums[i][j + 1]);
-                }
-                else break;
-            }
-            return ans;
+            pq.push({nums[i][0], i, 0});
+            maxi = max(maxi, nums[i][0]);
         }
+        while(true)
+        {
+            auto v = pq.top();
+            pq.pop();
+            int i = v[1], j = v[2], val = v[0];
+            mini = val;
+            if(maxi-mini < ans[1] - ans[0] || (maxi-mini == ans[1] - ans[0] && mini<ans[0]))
+            {
+                ans[0]=mini;
+                ans[1]=maxi;
+            }
+            if(j+1 < nums[i].size())
+            {
+                pq.push({nums[i][j+1],i,j+1});
+                maxi = max(maxi,nums[i][j+1]);
+            }
+            else break;
+        }
+        return ans;
+        
+    }
 };
