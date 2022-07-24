68e2938b2c00dcd3fe99e8777d2a92a2ccb7d99d
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-24 18:25:31 +0530

    Time: 218 ms (5.01%), Space: 59.7 MB (5.79%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -3,35 +3,67 @@ class Solution
     public:
         int peakIndexInMountainArray(vector<int> &nums)
         {
+            // int n = nums.size();
+            // if (n == 1) return 0;
+            // int low = 0, high = n - 1;
+            // while (low <= high)
+            // {
+            //     int mid = low + (high - low) / 2;
+            //     if (mid > 0 && mid < n - 1)
+            //     {
+            //         if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
+            //         else
+            //         {
+            //             if (nums[mid + 1] > nums[mid - 1]) low = mid + 1;
+            //             else high = mid - 1;
+            //         }
+            //     }
+            //     else
+            //     {
+            //         if (mid == 0)
+            //         {
+            //             if (nums[mid] > nums[mid + 1]) return mid;
+            //             else low = mid + 1;
+            //         }
+            //         else
+            //         {
+            //             if (nums[mid] > nums[mid - 1]) return mid;
+            //             else high = mid - 1;
+            //         }
+            //     }
+            // }
+            // return -1;
             int n = nums.size();
-            if (n == 1) return 0;
-            int low = 0, high = n - 1;
-            while (low <= high)
+            int low = 0, high = n-1;
+            while(low<=high)
             {
-                int mid = low + (high - low) / 2;
-                if (mid > 0 && mid < n - 1)
+                int mid = low + (high-low)/2;
+                int left_val;
+                if(mid-1 >=0)
                 {
-                    if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
-                    else
-                    {
-                        if (nums[mid + 1] > nums[mid - 1]) low = mid + 1;
-                        else high = mid - 1;
-                    }
+                    left_val = nums[mid-1];
+                }
+                else left_val = INT_MIN;
+                int right_val;
+                if(mid+1<n)
+                {
+                    right_val = nums[mid+1];
+                }
+                else right_val = INT_MIN;
+                if(left_val<nums[mid] && nums[mid]<right_val)
+                {
+                    low = mid+1;
+                }
+                else if(left_val>nums[mid] && nums[mid]>right_val)
+                {
+                    high = mid-1;
                 }
                 else
                 {
-                    if (mid == 0)
-                    {
-                        if (nums[mid] > nums[mid + 1]) return mid;
-                        else low = mid + 1;
-                    }
-                    else
-                    {
-                        if (nums[mid] > nums[mid - 1]) return mid;
-                        else high = mid - 1;
-                    }
+                    return mid;
                 }
             }
             return -1;
+
         }
 };
