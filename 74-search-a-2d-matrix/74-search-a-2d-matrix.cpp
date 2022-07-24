3830fc2db95aceb901e951b80ac4ca170b7d41e2
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-24 11:09:48 +0530

    Time: 6 ms (60.03%), Space: 9.7 MB (5.10%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -3,15 +3,44 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size(), n = matrix[0].size();
         
+        // binary search as 1d array
+        // int low = 0, high = m*n -1;
+        // while(low<=high)
+        // {
+        //     int mid = low + (high-low)/2;
+        //     int val = matrix[mid/n][mid%n];
+        //     if(val==target) return true;
+        //     else if(val>target) high=mid-1;
+        //     else low=mid+1;
+        // }
+        // return false;
         
-        int low = 0, high = m*n -1;
+        int rowans=-1;
+        int low = 0, high = m-1;
         while(low<=high)
         {
             int mid = low + (high-low)/2;
-            int val = matrix[mid/n][mid%n];
+            int val = matrix[mid][0];
+            if(target==val) return true;
+            else if(target > val)
+            {
+                rowans = mid;
+                low = mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        if(rowans==-1) return false;
+        low = 0, high=n-1;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            int val = matrix[rowans][mid];
             if(val==target) return true;
-            else if(val>target) high=mid-1;
-            else low=mid+1;
+            else if(val >  target) high=mid-1;
+            else low = mid+1;
         }
         return false;
     }
