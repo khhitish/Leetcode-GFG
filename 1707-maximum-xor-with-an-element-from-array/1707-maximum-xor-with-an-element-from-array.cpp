6725266b2bccee1a95cc15fdc93ad090126a446b
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-25 12:25:55 +0530

    Time: 2235 ms (6.23%), Space: 399.9 MB (5.03%) - LeetHub

--- 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.cpp
+++ 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.cpp
@@ -1,104 +1,102 @@
-// https://youtu.be/Q8LhG9Pi5KM
 struct Node
 {
-    Node *links[2];
+    vector<Node*> links = vector<Node*> (2);
+    
     bool containsKey(int bit)
     {
-        if (links[bit] != nullptr) return true;
-        else return false;
+        if(links[bit]==nullptr) return false;
+        return true;
     }
-
+    
     Node* get(int bit)
     {
         return links[bit];
     }
-
-    void put(int bit, Node *node)
+    
+    void put(int bit, Node* node)
     {
         links[bit] = node;
     }
 };
+
 class Trie
 {
     private:
-        Node * root;
+    Node* root;
     public:
-        Trie()
-        {
-            root = new Node();
-        }
-
+    Trie()
+    {
+        root=new Node();
+    }
+    
     void insert(int num)
     {
-        Node *dummy = root;
-        for (int i = 31; i >= 0; i--)
+        Node* curr = root;
+        for(int i = 31;i>=0;i--)
         {
-            int bit = (num >> i) &(1);
-            if (dummy->containsKey(bit) == false)
+            int bit = ((num>>i) & 1);
+            if(curr->containsKey(bit)==false)
             {
-                dummy->put(bit, new Node());
+                curr->put(bit, new Node());
             }
-            dummy = dummy->get(bit);
+            curr = curr->get(bit);
         }
     }
-
+    
     int getmax(int x)
     {
-        Node *dummy = root;
-        int ans = 0;
-        for (int i = 31; i >= 0; i--)
+        int ans=0;
+        Node* curr = root;
+        for(int i=31;i>=0;i--)
         {
-            int bit = (x >> i) &1;
-            if (dummy->containsKey(bit ^ 1) == true)
+            int bit = ((x>>i) & 1);
+            if(curr->containsKey(bit^1))
             {
-                ans = ans | (1 << i);
-                dummy = dummy->get(bit ^ 1);
+                ans|=(1<<i);
+                curr = curr->get(bit^1);
             }
             else
             {
-                dummy = dummy->get(bit);
+                curr = curr->get(bit);
             }
         }
         return ans;
     }
 };
-class Solution
-{
-    public:
-        vector<int> maximizeXor(vector<int> &nums, vector<vector< int>> &queries)
+class Solution {
+public:
+    vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
+        Trie *t;
+        t = new Trie();
+        int n=nums.size();
+        vector<vector<int>> offlinequeries;
+        for(int i = 0;i<queries.size();i++)
         {
-            Trie *t = new Trie();
-
-            sort(nums.begin(), nums.end());
-            vector<vector < int>> offlinequeries;
-            for (int i = 0; i < queries.size(); i++)
-            {
-                offlinequeries.push_back({ queries[i][1],queries[i][0],i });
-            }
-            sort(offlinequeries.begin(), offlinequeries.end());
-            int arrindex = 0;
-            vector<int> ansarr(queries.size());
-            for (int i = 0; i < offlinequeries.size(); i++)
-            {
-                int maxi = offlinequeries[i][0];
-                int x = offlinequeries[i][1];
-                int index = offlinequeries[i][2];
-
-                while (arrindex < nums.size() && nums[arrindex] <= maxi)
-                {
-                    t->insert(nums[arrindex]);
-                    arrindex++;
-                }
-                if (arrindex == 0)	// nothing smaller or equal to maxi, trie empty return -1
-                {
-                    ansarr[index] = -1;
-                }
-                else
-                {
-                    int ans = t->getmax(x);
-                    ansarr[index] = ans;
-                }
-            }
-            return ansarr;
+            offlinequeries.push_back({queries[i][1],queries[i][0],i});
         }
+        sort(nums.begin(), nums.end());
+        sort(offlinequeries.begin(), offlinequeries.end());
+        int arrindex=0;
+        vector<int> ans(queries.size());
+        for(auto&x : offlinequeries)
+        {
+            int maxi = x[0];
+            int num = x[1];
+            int ind = x[2];
+            while(arrindex<n && nums[arrindex]<=maxi)
+            {
+                t->insert(nums[arrindex]);
+                arrindex++;
+            }
+            if(arrindex==0)
+            {
+                ans[ind]=-1;
+            }
+            else
+            {
+                ans[ind]= t->getmax(num);
+            }
+        }
+        return ans;
+    }
 };
