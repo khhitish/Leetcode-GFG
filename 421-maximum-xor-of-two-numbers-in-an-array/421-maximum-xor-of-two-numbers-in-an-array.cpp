f582f7660472e96e19445ee353e5c28e292d56ef
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-25 11:53:10 +0530

    Time: 888 ms (18.25%), Space: 107.2 MB (19.96%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,83 +1,84 @@
-// https://youtu.be/EIhAwfHubE8
 struct Node
 {
-    Node *links[2];
+    vector<Node*> links = vector<Node*> (2);
+    
     bool containsKey(int bit)
     {
-        if (links[bit] != nullptr) return true;
-        else return false;
+        if(links[bit]==nullptr) return false;
+        return true;
     }
-
+    
     Node* get(int bit)
     {
         return links[bit];
     }
-
-    void put(int bit, Node *node)
+    
+    void put(int bit, Node* node)
     {
         links[bit] = node;
     }
 };
+
 class Trie
 {
     private:
-        Node * root;
+    Node* root;
     public:
-        Trie()
-        {
-            root = new Node();
-        }
-
+    Trie()
+    {
+        root=new Node();
+    }
+    
     void insert(int num)
     {
-        Node *dummy = root;
-        for (int i = 31; i >= 0; i--)
+        Node* curr = root;
+        for(int i = 31;i>=0;i--)
         {
-            int bit = (num >> i) &(1);
-            if (dummy->containsKey(bit) == false)
+            int bit = ((num>>i) & 1);
+            if(curr->containsKey(bit)==false)
             {
-                dummy->put(bit, new Node());
+                curr->put(bit, new Node());
             }
-            dummy = dummy->get(bit);
+            curr = curr->get(bit);
         }
     }
-
+    
     int getmax(int x)
     {
-        Node *dummy = root;
-        int ans = 0;
-        for (int i = 31; i >= 0; i--)
+        int ans=0;
+        Node* curr = root;
+        for(int i=31;i>=0;i--)
         {
-            int bit = (x >> i) &1;
-            if (dummy->containsKey(bit ^ 1) == true)
+            int bit = ((x>>i) & 1);
+            if(curr->containsKey(bit^1))
             {
-                ans = ans | (1 << i);
-                dummy = dummy->get(bit ^ 1);
+                ans|=(1<<i);
+                curr = curr->get(bit^1);
             }
             else
             {
-                dummy = dummy->get(bit);
+                curr = curr->get(bit);
             }
         }
         return ans;
     }
 };
-class Solution
-{
-    public:
-        int findMaximumXOR(vector<int> &nums)
+
+
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        Trie *t  =  new Trie();
+        for(int i = 0;i<nums.size();i++)
         {
-            Trie *t = new Trie();
-            for (int i = 0; i < nums.size(); i++)
-            {
-                t->insert(nums[i]);
-            }
-            int ans = INT_MIN;
-            for (int i = 0; i < nums.size(); i++)
-            {
-                int curr = nums[i];
-                ans = max(ans, t->getmax(curr));
-            }
-            return ans;
+            t->insert(nums[i]);
         }
+        int ans = 0;
+        for(int i = 0;i<nums.size();i++)
+        {
+            int temp = t->getmax(nums[i]);
+            ans = max(ans,temp);
+        }
+        return ans;
+    }
 };
