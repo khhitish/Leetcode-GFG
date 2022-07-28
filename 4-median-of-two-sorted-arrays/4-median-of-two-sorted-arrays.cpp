1b0fc0bfb57f0e50dfcf4a1a3509e200cc3c6914
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-07-29 02:57:20 +0530

    Time: 40 ms (90.43%), Space: 89.3 MB (86.23%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,53 +1,38 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m=nums1.size(), n=nums2.size();
-        int lim = (m+n)/2;
-        int low=lim-n<0? 0 : lim-n,high=min(lim,m);
-        bool iseven = (m+n)%2==0 ? true : false;
-        //cout<<iseven<<endl;
+        int m = nums1.size(), n = nums2.size();
+        bool iseven = false;
+        if((m+n) % 2 == 0) iseven = true;
+        int req = (m+n)/2;
+        int low = req<=n ? 0 : req-n;
+        int high = min(m,req);
         while(low<=high)
         {
-            int mid = low + (high-low)/2;
-            // if(mid>m)
-            // {
-            //     high=mid-1;
-            //     continue;
-            // }
-            // else if(lim-mid > n)
-            // {
-            //     low=mid+1;
-            //     continue;
-            // }
-            int l1,l2,r1,r2;
-            l1 = mid==0? INT_MIN : nums1[mid-1];
-            l2 = lim-mid==0 ? INT_MIN : nums2[lim-mid-1];
-            r1 = mid==m? INT_MAX : nums1[mid];
-            r2 = lim-mid==n? INT_MAX : nums2[lim-mid];
-            //cout<<mid<<" "<<l1<<" "<<r1<<" "<<l2<<" "<<r2<<" ";
-            //cout<<endl;
-            if(l1>r2)
+            int mid = low + (high - low)/2;
+            //mid represents the number of elements to be taken from arr1 into left half, req-mid represents the number of elements to be taken from arr2 into left half
+            int l1, l2, r1, r2;
+            l1 = mid==0 ? INT_MIN : nums1[mid-1]; // no elements picked from arr1 into left half or not
+            l2 = req-mid==0 ? INT_MIN : nums2[req - mid - 1]; // no elements picked from arr2 into left half or not
+            r1 = mid==m ? INT_MAX : nums1[mid]; //all elements of arr1 went into left half or not
+            r2 = req-mid==n ? INT_MAX : nums2[req - mid]; //all elements of arr2 went into left half or not
+            if(l1 > r2)
             {
-                high=mid-1;
+                high = mid-1;
             }
-            else if(l2>r1)
+            else if(l2 > r1)
             {
-                low=mid+1;
+                low = mid+1;
             }
             else
             {
                 if(iseven)
                 {
-                    double median = (max(l1,l2) + min(r1,r2))/2.0;
-                    return median;
+                    return (max(l1,l2) + min(r1,r2))/2.0;
                 }
-                else
-                {
-                    return min(r1,r2);
-                }
+                else return min(r1,r2);
             }
         }
-        
         return -1;
     }
 };
