7eb835ec9a9966a1d97205ab3794c71b08a973d8
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-08-02 09:46:16 +0530

    Time: 30 ms (94.96%), Space: 13 MB (92.86%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -8,9 +8,18 @@ public:
         {
             int mid = low + (high - low)/2;
             int count=0;
-            for(int i=0;i<n;i++)
+            // for(int i=0;i<n;i++)
+            // {
+            //     count += lower_bound(matrix[i].begin(), matrix[i].end(),mid) - matrix[i].begin();
+            // }
+            // O(2n) counting like search in 2d matrix 2
+            int r = 0, c=n-1;
+            while(r<n && c>=0)
             {
-                count += lower_bound(matrix[i].begin(), matrix[i].end(),mid) - matrix[i].begin();
+                while(c>=0 && matrix[r][c] >= mid) c--;
+                count+=(c+1);
+                //if(mid==12)cout<<c<<endl;
+                r++;
             }
             if(count<k)
             {
