432940125aabde0a73a830b1ff255718ce1020ba
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-08-02 09:34:40 +0530

    Time: 38 ms (83.41%), Space: 13.1 MB (77.54%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,42 +1,27 @@
-class Solution
-{
-    public:
-        int kthSmallest(vector<vector < int>> &matrix, int k)
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        int low = matrix[0][0], high = matrix[n-1][n-1];
+        int ans=-1;
+        while(low<=high)
         {
-            int n = matrix.size(), m = matrix[0].size();
-            int smaller = k-1;
-            int low = -1e9, high = 1e9;
-            int ans = -1;
-            while (low <= high)
+            int mid = low + (high - low)/2;
+            int count=0;
+            for(int i=0;i<n;i++)
             {
-                int mid = low + (high - low) / 2;
-                int countsmaller = 0, exists = 0;
-                for (int i = 0; i < n; i++)
-                {
-                    int index = lower_bound(matrix[i].begin(), matrix[i].end(), mid) - 
-                        matrix[i].begin();
-                    if (index< m && matrix[i][index] == mid) exists = 1;
-                    countsmaller += (index);
-                }
-                //cout<<mid<<" "<<countsmaller<<" ";
-                if (countsmaller > smaller)
-                {
-                    high = mid - 1;
-                }
-                else if (countsmaller < smaller)
-                {
-                    if (exists == 1) ans = mid;
-                    low = mid + 1;
-                }
-                else
-                {
-                    if (exists == 1) return mid;
-                    else
-                    {
-                        low = mid + 1;
-                    }
-                }
+                count += lower_bound(matrix[i].begin(), matrix[i].end(),mid) - matrix[i].begin();
             }
-            return ans;
+            if(count<k)
+            {
+                ans = mid;
+                low = mid+1;
+            }
+            else
+            {
+                high =  mid-1;
+            }
         }
+        return ans;
+    }
 };
