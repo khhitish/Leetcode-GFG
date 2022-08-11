c566853c0a91295b038e9988f4492eb0ea3ef1a3
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-08-11 22:21:27 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (32.60%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        // dp ncr approach;
+    vector<vector<int>> generate(int n) {
         vector<vector<int>> ans;
-        vector<int> prev={1};
+        vector<int> prev = {1};
         ans.push_back(prev);
-        for(int i=1;i<numRows;i++)
+        for(int i = 1;i < n; i++)
         {
             vector<int> temp(i+1);
-            temp[0]=temp[i]=1;
-            for(int j=1;j<i;j++)
+            temp[0] = temp[i]=1;
+            for(int j = 1;j<i;j++)
             {
                 temp[j] = prev[j-1] + prev[j];
             }
-            ans.push_back(temp);
             prev=temp;
+            ans.push_back(prev);
         }
         return ans;
     }
