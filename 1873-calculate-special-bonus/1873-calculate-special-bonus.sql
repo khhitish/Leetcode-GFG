a5bf43928e7153061729114c82737312f928780d
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-08-12 11:48:04 +0530

    Time: 630 ms (63.17%), Space: 0B (100.00%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -0,0 +1,3 @@
+
+# SELECT employee_id, if(MOD(employee_id,2)=1 AND name NOT LIKE 'M%',salary,0) AS bonus FROM Employees;
+SELECT employee_id, (CASE WHEN MOD(employee_id,2)=1 AND name NOT LIKE 'M%' THEN salary ELSE 0 END) AS bonus FROM Employees ORDER BY employee_id;
