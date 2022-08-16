ba8f714efef92122ac668ea783acde56220f3be3
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-08-16 10:06:04 +0530

    Time: 889 ms (17.14%), Space: 0B (100.00%) - LeetHub

--- 197-rising-temperature/197-rising-temperature.sql
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -1,2 +1,5 @@
-SELECT a.id as 'Id' 
-FROM Weather a INNER JOIN Weather b ON DATEDIFF(a.recordDate,b.recordDate) = 1 AND a.temperature > b.temperature;
+# SELECT a.id as 'Id' 
+# FROM Weather a INNER JOIN Weather b ON DATEDIFF(a.recordDate,b.recordDate) = 1 AND a.temperature > b.temperature;
+
+SELECT id FROM Weather a
+WHERE a.temperature > (SELECT b.temperature FROM Weather b WHERE DATEDIFF(a.recordDate,b.recordDate)=1);
