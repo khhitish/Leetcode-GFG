76cb50d4e6dfbb10ee476089228f660912f2599f
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-08-19 08:42:00 +0530

    Time: 164 ms (51.36%), Space: 58.4 MB (58.44%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        unordered_map<int,int> rem, end;
+        for(auto&x : nums)rem[x]++;
+        for(auto&x : nums)
+        {
+            if(rem[x]==0) continue;
+            rem[x]--;
+            if(end[x-1]>0)
+            {
+                end[x-1]--;
+                end[x]++;
+            }
+            else if(rem[x+1]>0 && rem[x+2]>0)
+            {
+                rem[x+1]--;
+                rem[x+2]--;
+                end[x+2]++;
+            }
+            else return false;
+        }
+        return true;
+    }
+};
