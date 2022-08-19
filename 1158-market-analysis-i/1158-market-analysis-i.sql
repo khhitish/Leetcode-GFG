de4f62893cf142d7d318bd6691636a7059da7573
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-08-19 09:35:51 +0530

    Time: 1429 ms (50.22%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,1 +1,6 @@
-SELECT user_id AS buyer_id, join_date, (SELECT COUNT(*) FROM Orders y WHERE x.user_id = y.buyer_id AND YEAR(y.order_date)='2019') AS orders_in_2019 FROM Users x;
+# SELECT user_id AS buyer_id, join_date, (SELECT COUNT(*) FROM Orders y WHERE x.user_id = y.buyer_id AND YEAR(y.order_date)='2019') AS orders_in_2019 FROM Users x;
+# SELECT * FROM Orders WHERE YEAR(order_date)='2019';
+SELECT u.user_id AS buyer_id, u.join_date, IFNULL(COUNT(o.order_id),0) AS orders_in_2019
+FROM Users u LEFT JOIN Orders o ON u.user_id = o.buyer_id AND YEAR(o.order_date)='2019'
+GROUP BY u.user_id;
+# HAVING YEAR(o.order_date)='2019';
