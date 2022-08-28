cbf137d9244774d62e32a07e007658be0132f20b
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-08-28 09:11:12 +0530

    Time: 13 ms (77.01%), Space: 9.3 MB (52.67%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -2,26 +2,44 @@ class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
         int m =mat.size(), n=mat[0].size();
-        int i=m-1, j=0;
-        while(j<n)
+        // int i=m-1, j=0;
+        // while(j<n)
+        // {
+        //     int x = i, y=j;
+        //     vector<int> v;
+        //     while(x<m && y<n)
+        //     {
+        //         v.push_back(mat[x][y]);
+        //         x++; y++;
+        //     }
+        //     sort(v.begin(), v.end());
+        //     x = i, y=j;
+        //     int t = 0;
+        //     while(x<m && y<n)
+        //     {
+        //         mat[x][y] = v[t];
+        //         x++;y++;t++;
+        //     }
+        //     if(i>0) i--;
+        //     else j++;
+        // }
+        // return mat;
+        
+        unordered_map<int,priority_queue<int,vector<int>,greater<int>>> mpp;
+        for(int i=0;i<m;i++)
         {
-            int x = i, y=j;
-            vector<int> v;
-            while(x<m && y<n)
+            for(int j=0;j<n;j++)
             {
-                v.push_back(mat[x][y]);
-                x++; y++;
+                mpp[i-j].push(mat[i][j]);
             }
-            sort(v.begin(), v.end());
-            x = i, y=j;
-            int t = 0;
-            while(x<m && y<n)
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
             {
-                mat[x][y] = v[t];
-                x++;y++;t++;
+                mat[i][j] = mpp[i-j].top();
+                mpp[i-j].pop();
             }
-            if(i>0) i--;
-            else j++;
         }
         return mat;
     }
