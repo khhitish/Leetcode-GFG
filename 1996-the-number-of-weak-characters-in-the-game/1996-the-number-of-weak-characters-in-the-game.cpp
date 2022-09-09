dc29d31a812963c168f1fd980605cdd1130d7dde
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-09-09 21:49:03 +0530

    Time: 1034 ms (57.07%), Space: 125.1 MB (70.41%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,26 +1,33 @@
-class Solution
-{
+class Solution {
 public:
-     static bool comp(vector<int> &a, vector<int> &b)
-     {
-          if (a[0] == b[0])
-          {
-               return a[1] > b[1];
-          }
-          return a[0] < b[0];
-     }
-     int numberOfWeakCharacters(vector<vector<int>> &properties)
-     {
-          sort(properties.begin(), properties.end(), comp);
-          int mtn = INT_MIN;  
-          int ans = 0;
-
-          for (int i = properties.size() - 1; i >= 0; i--)
-          {
-               if (properties[i][1] < mtn)
+    int numberOfWeakCharacters(vector<vector<int>>& p) {
+        int n = p.size();
+        sort(p.begin(), p.end());
+        // for(auto&x : p)
+        // {
+        //     cout<<x[0]<<" "<<x[1]<<endl;
+        // }
+        // cout<<endl;
+        int i = n-1, ans=0;
+        int attack=INT_MIN, defense=INT_MIN;
+        while(i>=0)
+        {
+            int newdef=INT_MIN;
+            int temp=p[i][0];
+            while(i>=0 && p[i][0] == temp)
+            {
+                newdef=max(newdef,p[i][1]);
+                if(defense > p[i][1] && attack>p[i][0])
+                {
                     ans++;
-               mtn = max(mtn, properties[i][1]);
-          }
-          return ans;
-     }
+                    // cout<<defense<<" "<<p[i][0] <<" "<<p[i][1]<<endl;
+                }
+                i--;
+            }
+            defense =max(newdef,defense);
+            attack=p[i+1][0];
+        }
+        return ans;
+        
+    }
 };
