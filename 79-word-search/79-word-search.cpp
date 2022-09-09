6df6b9505d3b2c1ab028c6e3fa8822eb9c7e855b
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-09-09 20:31:49 +0530

    Time: 358 ms (79.39%), Space: 8.1 MB (45.21%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,32 +1,29 @@
 class Solution {
 private:
-    bool helper(int i, int j, int idx, string& word, vector<vector<char>>& board)
+    bool helper(int idx, int i, int j, string& word, vector<vector<char>>& board, vector<int> &dir)
     {
-        
-        int m=board.size(), n=board[0].size(), sz = word.size();
-        if(idx==sz) return true;
+        if(idx==word.size()) return true;
+        int m=board.size(), n=board[0].size();
         if(i<0 || i>=m || j<0 || j>=n || board[i][j]!=word[idx]) return false;
-        board[i][j] = '#';
-        vector<int> dir = {0,-1,0,1,0};
+        board[i][j]='#';
+        
         for(int k=0;k<4;k++)
         {
-            int row = i + dir[k], col = j+ dir[k+1];
-            if(helper(row,col,idx+1,word,board)==true) return true;
+            int x=i+dir[k], y=j+dir[k+1];
+            if(helper(idx+1,x,y,word,board,dir)==true) return true;
         }
-        board[i][j] = word[idx];
+        board[i][j]=word[idx];
         return false;
     }
 public:
     bool exist(vector<vector<char>>& board, string word) {
         int m=board.size(), n=board[0].size();
+        vector<int> dir={0,1,0,-1,0};
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(board[i][j] == word[0])
-                {
-                    if(helper(i,j,0,word,board)==true) return true;
-                }
+                if(board[i][j]==word[0])if(helper(0,i,j,word,board,dir)==true) return true;
             }
         }
         return false;
