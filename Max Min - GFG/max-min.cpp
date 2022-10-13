6d7b97cc29dd03e9f0f05eb0d8dc52d3d9bd639d
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2022-10-13 22:43:00 +0530

    Added solution - LeetHub

---
+++ Max Min - GFG/max-min.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+   public:
+    int findSum(int a[], int n)
+    {
+    	int maxi = INT_MIN, mini = INT_MAX;
+    	for(int i=0;i<n;i++)
+    	{
+    	    maxi = max(a[i],maxi);
+    	    mini = min(a[i],mini);
+    	}
+    	return maxi + mini;
+    }
+
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	      cin>>arr[i];
+	    Solution ob;  
+	    int ans=ob.findSum(arr, n);
+	    cout<<ans;
+	    cout<<"\n";
+	}
+	return 0;
+}
+
+// } Driver Code Ends
