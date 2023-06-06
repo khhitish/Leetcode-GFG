e9179fc7927317a3e686f1f99a4302753de17052
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2023-06-06 22:57:30 +0530

    Time: 0 ms (100%), Space: 8.9 MB (92.44%) - LeetHub

--- 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.cpp
+++ 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.cpp
@@ -1,10 +1,24 @@
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        int d = arr[1] - arr[0];
-        for(int i=2;i<arr.size();i++) {
-            if(d!=arr[i] - arr[i-1]) return false;
+        int mini = arr.front(), maxi = arr.front();
+        for(auto&x : arr) {
+            maxi = max(maxi,x);
+            mini = min(mini,x);
+        }
+        if(maxi == mini) return true;
+        int n = arr.size();
+        if((maxi - mini)%(n-1)!=0) return false;
+        int d = (maxi - mini)/(n-1);
+        int i = 0;
+        while(i<n) {
+            if(((arr[i] - mini) % d) != 0)return false;
+            else if(((arr[i] - mini)/d)==i)i++;
+            else {
+                int pos = (arr[i] - mini) / d;
+                if (arr[pos] == arr[i]) return false;
+                swap(arr[i], arr[pos]);
+            }
         }
         return true;
     }
