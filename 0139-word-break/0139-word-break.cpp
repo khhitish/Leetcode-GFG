615c4a76dc64477364db4da755745ffa6772a5d1
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2023-06-12 09:05:07 +0530

    Time: 5 ms (79.67%), Space: 8.2 MB (75.91%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,27 +1,26 @@
 class Solution {
 private:
-    bool helper(int i, int last, string& s, unordered_set<string>& st, vector<vector<int>>& dp) {
+    bool helper(int i, string& s, unordered_set<string>& st, vector<int>& dp) {
         int n = s.size();
-        if(i == n) {
-            if(last == n) return true;
-            return false;
+        if(i == n) return true;
+        if(dp[i]!=-1) return dp[i];
+        string temp;
+        for(int j=i;j<n;j++) {
+            temp+=s[j];
+            if(st.count(temp)) {
+                if(helper(j+1,s,st,dp)) return dp[i] = true;
+            }
         }
-        if(dp[i][last]!=-1) return dp[i][last];
-        bool ans = false;
-        if(st.count(s.substr(last,i-last+1))) {
-            ans|=helper(i+1,i+1,s,st,dp);
-        }
-        ans|=helper(i+1,last,s,st,dp);
-        return dp[i][last] = ans;
+        return dp[i] = false;
     }
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> st;
         int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n,-1));
+        vector<int> dp(n, -1);
         for(auto&x : wordDict) {
             st.insert(x);
         }
-        return helper(0,0,s,st,dp);
+        return helper(0,s,st,dp);
     }
 };
