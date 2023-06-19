bd09a36dc1a0349f59290caf3bbc12fbc911af0e
Khitish Agrawalla <106798309+khhitish@users.noreply.github.com>
2023-06-19 20:24:48 +0530

    Time: 72 ms (42.28%), Space: 42.3 MB (61.44%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
     int maxScore(vector<int>& nums, int k) {
-        int n = nums.size();
-        int fullSum = 0;
-        for(auto &x : nums) fullSum += x;
-        int sz = n-k;
-        if(sz == 0) return fullSum;
-        int currSum = 0, minSum = INT_MAX;
-        for(int i=0;i<n;i++) {
-            currSum+=nums[i];
-            if(i == sz-1) {
-                minSum = min(minSum, currSum);
-            }
-            if(i >= sz) {
-                currSum -= nums[i-sz];
-                minSum = min(minSum, currSum);
-            }
+        // // Minimum subarray sum of size n-k
+        // int n = nums.size();
+        // int fullSum = 0;
+        // for(auto &x : nums) fullSum += x;
+        // int sz = n-k;
+        // if(sz == 0) return fullSum;
+        // int currSum = 0, minSum = INT_MAX;
+        // for(int i=0;i<n;i++) {
+        //     currSum+=nums[i];
+        //     if(i == sz-1) {
+        //         minSum = min(minSum, currSum);
+        //     }
+        //     if(i >= sz) {
+        //         currSum -= nums[i-sz];
+        //         minSum = min(minSum, currSum);
+        //     }
+        // }
+        // return fullSum - minSum;
+
+        // Direct implementation of the question
+        int sum = 0, ans=0;
+        for(int i=0;i<k;i++) sum+=nums[i];
+        ans=max(ans,sum);
+        int i=k,j=nums.size();
+        while(i>0)
+        {
+            i--;j--;
+            sum-=nums[i];
+            sum+=nums[j];
+            ans=max(ans,sum);
         }
-        return fullSum - minSum;
+        return ans;
     }
 };
